988d8fd39d3cb01ce0987b74aaacee1ff30b928c
Alex Reyes <akosialexpo@gmail.com>
2013-07-24 08:06:07 +0800

    Completed correct solution with brute force

--- 47.py
+++ 47.py
@@ -3,26 +3,37 @@
 
 import math
 
+
+def is_prime(num):
+    
+    for i in xrange(2, num):
+        if not num % i:
+            return False
+    return True
+
 def has_4_prime_factors(num):
 
     count = 0
 
-    for i in xrange(2, int(round(math.sqrt(num)))):
-        print "has_4_prime: ", num, i
-        if not num % i:
-            for j in xrange(2, int(round(math.sqrt(i)))):
-                if not i % j:
-                    count += 1
-    return count == 2
-    
+    print "Num: ", num
+
+    for i in xrange(2, num):
+        if not num % i and is_prime(i):
+            print "Prime factor: ", i
+            count += 1
+            print "Count: ", count
+
+    return count == 4
+
 def find_consecutive_nums():
-    while True:
-        num = 10
-        print "find_consec: ", num
-        if has_4_prime_factors(num) and has_4_prime_factors(num + 1): # and has_4_prime_factors(num + 2) and has_4_prime_factors(num + 3):
-            return num
+    num = 5
+    while num < 134500:
+        if has_4_prime_factors(num) and has_4_prime_factors(num + 1) and has_4_prime_factors(num + 2) and has_4_prime_factors(num + 3):
+            return (num, num + 1, num + 2, num + 3)
 
         num += 1
+    
+    return "No answer found"
 
 if __name__ == '__main__':
     print "Answer found: ", find_consecutive_nums()
