d0bb2b3ab4ef63595a6e62b09f87cb17dad66642
Alex Reyes <akosialexpo@gmail.com>
2013-08-04 12:49:02 +0800

    Emulated dictionary type

--- 21.cpp
+++ 21.cpp
@@ -7,7 +7,7 @@ int divisorSum(int num) {
 
     int sum = 0;
 
-    for (int n = 0; n < num; n++) {
+    for (int n = 0; n < num/2 + 1; n++) {
         if (!(num % n))
             sum += n;
     }
@@ -18,16 +18,21 @@ int divisorSum(int num) {
 
 int amicablePair(int limit) {
 
-    int sum = 0;
+    int sum = 0, pairs[2][limit];
 
-    for (int i = 10; i < limit; i++) {
-
-    for (int j = 10; j < limit; i++) {
-       if ((divisorSum(i) == divisorSum(j)) && (i != j)) {
-           sum += divisorSum(i);
+    // Store sum of divisors
+    for (int i = 0; i < limit - 1; i++) {
+        for (int j = 0; j < limit - 1; j++) {
+            pairs[0][i+1] = i+1;
+            pairs[1][j+1] = divisorSum(i+1);
         }
     }
 
+    // Iterate list and find pairs
+    for (int num = 0; num < limit; num++) {
+        if (pairs[1][num] == num && pairs[0][num] != num)
+            sum += num;
+    }
 
     return sum;
 }
