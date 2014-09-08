367dda3caf182075f164a1ad3bab0e7500e432c7
Alex <akosialexpo@gmail.com>
2014-09-08 23:31:04 +0800

    Complete solution

--- 43.rb
+++ 43.rb
@@ -1,48 +1,44 @@
-def isPandigital(num)
-    for i in 0..9
-        unless (num.to_s.include?(i.to_s))
-            return false
+def hasProperty(num)
+    # Splices
+    for i in 1..$div.length
+        puts "#{num[i]}, #{num[i+1]}, #{num[i+2]} div #{$div[i-i]}"
+        if num[i+2]
+            unless ((num[i] + num[i+1] + num[i+2]).to_i % $div[i-1] == 0)
+                return false
+            end
         end
     end
     return true
 end
 
-def hasProperty(num)
-
-    # Create str array from int
-    num.to_s.each_char do |i|
-        (@s_num ||= []) << i
-    end
-
-    # Splices
-    div = [2, 3, 5, 7, 11, 13, 17]
-    for i in 1..div.length
-        unless ((@s_num[i] + @s_num[i+1] + @s_num[i+2]).to_i % div[i-1] == 0)
-            return false
-        end
-        puts "#{@s_num[i..i+2]}, div = #{div[i-1]}"
-    end
-    return true
+#Get all pandigital numbers
+def get_pandigitals
+    return (0..9).to_a.permutation.map(&:join)
 end
 
 
 #Main
-#i = 1406357289
-#for i in 1406357270..1406357390
-for i in 1000000000..9999999999
-    if i % 212311 == 0
-        puts "Trying #{i}..."
-    end
-    if isPandigital(i)
-       if hasProperty(i)
-           puts "Found! #{i}"
-           ($pans ||= []) << i
-       end
+puts "Started!"
+$div = [2, 3, 5, 7, 11, 13, 17]
+$pans = get_pandigitals
+puts "Got list of permutations!"
+
+puts "Cleaning list of invalid ints..."
+#Remove strings starting with '0'
+$pans.delete_if { |x| x[0] == '0' }
+puts "Cleaned list!"
+
+puts "Finding appropriate ints..."
+#Find int with property
+$pans.each do |i|
+    if hasProperty(i)
+        puts "Found! #{i}"
+        ($pans_sum ||= []) << i.to_i
     end
 end
 
-if $pans
-    puts "Answer found: #{$pans.inject(:+)}"
+if $pans_sum
+    puts "Answer found: #{$pans_sum.inject(:+)}"
 else
     puts "No answer found."
 end
